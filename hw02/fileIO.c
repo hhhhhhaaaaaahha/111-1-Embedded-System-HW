e8d9c343c24d065f00fdc58842f9f501fc88f894
Ray <raytu.tpe@gmail.com>
2022-12-17 18:38:46 +0800

    upload hw01 and hw02

---
+++ hw02/fileIO.c
@@ -0,0 +1,42 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+
+
+MODULE_DESCRIPTION("hello_world");
+MODULE_LICENSE("GPL");
+
+static int file_init(void)
+{
+	static char arr[262];
+	static char arr2[262];
+	loff_t pos = 0;
+	struct file *cfile;
+	struct file *cfile2;
+	cfile = filp_open("/home/ray/Desktop/fileIO/input.txt", O_RDONLY, 0);
+	if(IS_ERR(cfile)){
+		printk(KERN_INFO "File open failed !\n");
+	}
+	
+	kernel_read(cfile, arr, sizeof(arr), &pos);
+	cfile2 = filp_open("/home/ray/Desktop/fileIO/output.txt", O_CREAT|O_RDWR, 0644);
+	pos = 0;
+	int i=0;
+	int j=260;
+	for(i;i<strlen(arr);i++){
+		arr2[i] = arr[strlen(arr)-i-1];
+	}
+	kernel_write(cfile2, arr2, strlen(arr2), &pos);
+	
+	filp_close(cfile, NULL);
+	filp_close(cfile2, NULL);
+	return 0;
+}
+
+static void file_exit(void)
+{
+	printk(KERN_INFO "Bye !\n");
+}
+
+module_init(file_init);
+module_exit(file_exit);
